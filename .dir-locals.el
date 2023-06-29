8ee1665028e40b820db1ef78bc5f22b5a1058a02
João Távora <joaotavora@gmail.com>
2023-06-30 00:43:05 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eglot-server-programs . (((c++-mode c-mode) "clangd" "--query-driver=**"))))))
